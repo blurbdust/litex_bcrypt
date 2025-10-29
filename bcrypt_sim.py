dfd73cd49e2ec8f30b6fb8d6fb57a4a7ef97e37c
Florent Kermarrec <florent@enjoy-digital.fr>
2025-10-29 09:55:47 +0100

    gateware: Move AXIS8Streamer/AXIS8Recorder to their own module to avoid duplication in sim/target.

--- bcrypt_sim.py
+++ bcrypt_sim.py
@@ -30,11 +30,11 @@ from litex.soc.integration.soc      import SoCRegion
 from litex.soc.integration.soc_core import SoCMini
 from litex.soc.integration.builder  import Builder
 
-from litex.soc.interconnect     import stream, wishbone
-from litex.soc.interconnect.csr import CSRStorage, CSRStatus
+from litex.soc.interconnect import wishbone
 
 from liteeth.phy.model import LiteEthPHYModel
 
+from gateware.axis_8b import AXIS8Streamer, AXIS8Recorder
 from gateware.bcrypt_wrapper import BcryptWrapper
 
 # IOs ----------------------------------------------------------------------------------------------
@@ -64,121 +64,6 @@ _io = [
 class Platform(SimPlatform):
     def __init__(self):
         super().__init__("SIM", _io)
-
-# AXI Streamer (8-bit) -----------------------------------------------------------------------------
-
-class AXI8Streamer(LiteXModule):
-    def __init__(self, sram_mem):
-        self.source = source = stream.Endpoint([("data", 8)])
-
-        self.length = CSRStorage(32)
-        self.kick   = CSRStorage(1)
-        self.done   = CSRStatus(1)
-
-        # # #
-
-        # Signals.
-        addr     = Signal(32)
-        byte_sel = Signal(2)
-
-        # Kick edge detect.
-        kick_d = Signal()
-        self.sync += kick_d.eq(self.kick.storage)
-        start = self.kick.storage & ~kick_d
-
-        # SRAM Read Port.
-        port = sram_mem.get_port(async_read=True)
-        self.specials += port
-
-        # Data-Generation.
-        self.comb += [
-            port.adr.eq(addr[2:]),
-            byte_sel.eq(addr[0:2]),
-            source.last.eq(addr == (self.length.storage - 1)),
-            Case(byte_sel, {
-                0b00: source.data.eq(port.dat_r[ 0: 8]),
-                0b01: source.data.eq(port.dat_r[ 8:16]),
-                0b10: source.data.eq(port.dat_r[16:24]),
-                0b11: source.data.eq(port.dat_r[24:32]),
-            })
-        ]
-
-        # FSM.
-        self.fsm = fsm = FSM(reset_state="IDLE")
-        fsm.act("IDLE",
-            source.valid.eq(0),
-            self.done.status.eq(1),
-            If(start & (self.length.storage > 0),
-                NextValue(addr, 0),
-                NextState("RUN")
-            )
-        )
-        fsm.act("RUN",
-            source.valid.eq(1),
-            self.done.status.eq(0),
-            If(source.ready,
-                NextValue(addr, addr + 1),
-                If(source.last,
-                    NextState("IDLE")
-                )
-            )
-        )
-
-# AXI Recorder (8-bit) -----------------------------------------------------------------------------
-
-class AXI8Recorder(LiteXModule):
-    def __init__(self, sram_mem):
-        self.sink = stream.Endpoint([("data", 8)])
-
-        self.kick  = CSRStorage(1)
-        self.done  = CSRStatus (1)
-        self.count = CSRStatus (32)
-
-        # # #
-
-        # Signals.
-        byte_addr = Signal(32)
-        byte_cnt  = Signal(32)
-
-        # Kick edge detect.
-        kick_d = Signal()
-        self.sync += kick_d.eq(self.kick.storage)
-        start = self.kick.storage & ~kick_d
-
-        # SRAM Write Port.
-        port = sram_mem.get_port(write_capable=True, we_granularity=8)
-        self.specials += port
-        self.comb += port.adr.eq(byte_addr[2:])
-
-        # FSM.
-        self.fsm = fsm = FSM(reset_state="IDLE")
-        fsm.act("IDLE",
-            self.sink.ready.eq(0),
-            self.done.status.eq(1),
-            If(start,
-                NextValue(byte_addr, 0),
-                NextValue(byte_cnt,  0),
-                NextState("RUN")
-            )
-        )
-        fsm.act("RUN",
-            self.sink.ready.eq(1),
-            self.done.status.eq(0),
-            If(self.sink.valid,
-                Case(byte_addr[0:2], {
-                    0b00 : [port.dat_w[ 0: 8].eq(self.sink.data), port.we.eq(0b0001)],
-                    0b01 : [port.dat_w[ 8:16].eq(self.sink.data), port.we.eq(0b0010)],
-                    0b10 : [port.dat_w[16:24].eq(self.sink.data), port.we.eq(0b0100)],
-                    0b11 : [port.dat_w[24:32].eq(self.sink.data), port.we.eq(0b1000)],
-                }),
-                NextValue(byte_addr, byte_addr + 1),
-                NextValue(byte_cnt,  byte_cnt  + 1),
-                If(self.sink.last,
-                    NextState("IDLE")
-                )
-            )
-        )
-        self.comb += self.count.status.eq(byte_cnt)
 
 # Simulation SoC -----------------------------------------------------------------------------------
 
@@ -219,7 +104,7 @@ class SimSoC(SoCMini):
 
         # Streamer ---------------------------------------------------------------------------------
 
-        self.streamer = AXI8Streamer(self.streamer_sram.mem)
+        self.streamer = AXIS8Streamer(self.streamer_sram.mem)
 
         # Bcrypt Wrapper ---------------------------------------------------------------------------
 
@@ -239,7 +124,7 @@ class SimSoC(SoCMini):
 
         # Recorder ---------------------------------------------------------------------------------
 
-        self.recorder = AXI8Recorder(self.recorder_sram.mem)
+        self.recorder = AXIS8Recorder(self.recorder_sram.mem)
 
         # Streamer → Bcrypt → Recorder Datapaths ---------------------------------------------------
 
